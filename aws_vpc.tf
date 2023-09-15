60a25ab948e8e4e0080bf1689d1b3c21ae961b90
Chandu31313 <chandrashekar31313@gmail.com>
2023-09-15 15:11:10 +0530

    commited

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "ecoms1" {
+resource "aws_vpc" "ecomm-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "ecom-vpc"
+    Name = "master-vpc"
   }
 }
