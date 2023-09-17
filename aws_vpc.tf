2eb08cb0e4ed33480f1680a174984034ff510f00
Chandu31313 <chandrashekar31313@gmail.com>
2023-09-17 13:31:49 +0530

    public route table created

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -9,12 +9,51 @@ resource "aws_vpc" "ecomm-vpc" {
 
 # Public subnet
 
-resource "aws_subnet" "ecom-pb-sn" {
+resource "aws_subnet" "ecomm-pb-sn" {
   vpc_id     = aws_vpc.ecomm-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone="us-east-2a"
   map_public_ip_on_launch="true"
   tags = {
     Name = "ecom-public-subnet"
+  }
+}
+
+
+# Private subnet
+
+resource "aws_subnet" "ecomm-pvt-sn" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone="us-east-2b"
+  map_public_ip_on_launch="false"
+  tags = {
+    Name = "ecom-private-subnet"
+  }
+}
+
+# internet gateway
+resource "aws_internet_gateway" "ecomm-igw" {
+  vpc_id = aws_vpc.ecomm-vpc.id
+
+  tags = {
+    Name = "ecom-internet-gateway"
+  }
+}
+
+#public route table
+
+resource "aws_route_table" "ecomm-pub-rt" {
+  vpc_id = aws_vpc.ecomm-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ecomm-igw.id
+  }
+
+  
+
+  tags = {
+    Name = "ecomm-public-rout-table"
   }
 }
